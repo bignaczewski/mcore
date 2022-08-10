64f79268cc57b96eb4a53f6df5ee512854f708e2
Rodrigo Barreto <rodrigobarreto@rodrigos-mbp.lan>
2022-08-10 15:26:11 +0100

    fixing conflits

--- spec/app/facade/mcore/posts/index_facade_spec.rb
+++ spec/app/facade/mcore/posts/index_facade_spec.rb
@@ -80,7 +80,6 @@ RSpec.describe Mcore::Posts::IndexFacade do
         expect(index_facade.posts_filtered_by_title.posts.first.title).to eq title
       end
     end
-<<<<<<< HEAD
 
     describe '#two_titles_from_different_user' do
       subject(:index_facade) do
@@ -104,7 +103,5 @@ RSpec.describe Mcore::Posts::IndexFacade do
         expect(index_facade.posts_filtered_by_user.posts_filtered_by_title.posts.size).to eq 1
       end
     end
-=======
->>>>>>> ba63ade1778d350f31bf01a7b7fc657c23892c41
   end
 end
